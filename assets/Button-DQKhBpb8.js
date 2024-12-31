2dd5c55c109240cf91fb0513f4ea14aab4952f04
Alexandr Alexandrov <realumnyashkin@yandex.ru>
2024-12-31 17:58:47 +0300

    Updates

--- assets/Button-DQKhBpb8.js
+++
@@ -1,1 +0,0 @@
-import{j as _}from"./index-dF6jHRf7.js";const b="_btn_1jgzf_1",e="_withIcon_1jgzf_10",n={btn:b,withIcon:e},i=({text:s,icon:t,className:o="",...c})=>_.jsxs("button",{className:`${n.btn} ${o} ${t?n.withIcon:""}`,...c,children:[t&&t,s]});export{i as B};
